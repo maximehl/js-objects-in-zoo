52be737d22e7d8101f61a66bdb0e79a26a3ed7b1
maximehl <maximehendrikseliu@students.berkeley.net>
2018-09-06 12:41:32 -0700

    through problem 4

--- zoo.js
+++ zoo.js
@@ -1,13 +1,50 @@
 const run = () => {
-    sleep("Tigger");
+    let tigger = new Tiger("Tigger");
+    tigger.eat("meat");
+    tigger.eat("kibble");
+
+    let pooh = new Bear("Pooh");
+    pooh.eat("fish");
+    pooh.eat("meat");
 };
 
-let favoriteFood = "bacon";
-const sleep = (name) => {
-    console.log(name + " sleeps for 8 hours");
-};
+// const sleep = (name) => {
+//     console.log(name + " sleeps for 8 hours");
+// };
+//
+// const eat = (name, food) => {
+//     console.log(name + " eats " + food);
+//     food===favoriteFood ? console.log("YUM!!! " + name + " wants more " + food) : sleep(name);
+// };
 
-const eat = (name, food) => {
-    console.log(name + " eats " + food);
-    food===favoriteFood ? console.log("YUM!!! " + name + " wants more " + food) : sleep(name);
-};
+class Tiger{
+    constructor(name){
+        this.name = name;
+        this.favoriteFood = "meat";
+    }
+
+    sleep(){
+        console.log(this.name + " sleeps for 8 hours");
+    };
+
+    eat(food){
+        console.log(this.name + " eats " + food);
+        food===this.favoriteFood ? console.log("YUM!!! " + this.name + " wants more " + food) : this.sleep();
+    };
+}
+
+class Bear{
+    constructor(name){
+        this.name = name;
+        this.favoriteFood = "fish";
+    }
+
+    sleep(){
+        console.log(this.name + " hibernates for 4 months");
+    };
+
+    eat(food){
+        console.log(this.name + " eats " + food);
+        food===this.favoriteFood ? console.log("YUM!!! " + this.name + " wants more " + food) : this.sleep();
+    }
+}
