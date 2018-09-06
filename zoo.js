c19bca3ddb4b40830d82d27b81e539928f8060b6
maximehl <maximehendrikseliu@students.berkeley.net>
2018-09-06 13:12:18 -0700

    through the second problem 5

--- zoo.js
+++ zoo.js
@@ -1,11 +1,23 @@
 const run = () => {
     let tigger = new Tiger("Tigger");
     tigger.eat("meat");
-    tigger.eat("kibble");
+    //tigger.eat("kibble");
 
     let pooh = new Bear("Pooh");
     pooh.eat("fish");
     pooh.eat("meat");
+
+    let rarity = new Unicorn("Rarity");
+    rarity.eat("marshmallows");
+    rarity.sleep();
+
+    let gemma = new Giraffe("Gemma");
+    gemma.eat("meat");
+    gemma.eat("leaves");
+
+    let stinger = new Bee("Stinger");
+    stinger.eat("ice cream");
+    stinger.eat("pollen");
 };
 
 // const sleep = (name) => {
@@ -16,11 +28,10 @@ const run = () => {
 //     console.log(name + " eats " + food);
 //     food===favoriteFood ? console.log("YUM!!! " + name + " wants more " + food) : sleep(name);
 // };
-
-class Tiger{
-    constructor(name){
+class Animal{
+    constructor(name, favoriteFood){
         this.name = name;
-        this.favoriteFood = "meat";
+        this.favoriteFood = favoriteFood;
     }
 
     sleep(){
@@ -33,18 +44,62 @@ class Tiger{
     };
 }
 
-class Bear{
+class Tiger extends Animal{
     constructor(name){
-        this.name = name;
-        this.favoriteFood = "fish";
+        super(name, "meat");
+    }
+}
+
+class Bear extends Animal{
+    constructor(name){
+        super(name, "fish");
     }
 
     sleep(){
         console.log(this.name + " hibernates for 4 months");
     };
+}
+
+class Unicorn extends Animal{
+    constructor(name){
+        super(name, "marshmallows");
+    }
+
+    sleep(){
+        console.log(this.name + " sleeps in a cloud");
+    }
+}
+
+class Giraffe extends Animal{
+    constructor(name){
+        super(name, "leaves");
+    }
 
     eat(food){
-        console.log(this.name + " eats " + food);
-        food===this.favoriteFood ? console.log("YUM!!! " + this.name + " wants more " + food) : this.sleep();
+        if(food===this.favoriteFood){
+            super.eat(food);
+            this.sleep();
+        }else{
+            console.log("YUCK!!! " + this.name + " will not eat " + food);
+        }
+    }
+}
+
+class Bee extends Animal{
+    constructor(name){
+        super(name, "pollen");
+    }
+
+    sleep(){
+        console.log(this.name + " never sleeps");
+    }
+
+    eat(food){
+        if(food===this.favoriteFood){
+            super.eat(food);
+            this.sleep();
+        }else{
+            console.log("YUCK!!! " + this.name + " will not eat " + food);
+        }
     }
 }
